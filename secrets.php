6e429577090455a3d977ff1ab35a7355a11bd9c6
Helitha Guruge <145540187+iamhelitha@users.noreply.github.com>
2025-01-29 20:48:39 +0530

    Delete secrets.php

--- secrets.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'egs');
-define('DB_USER', 'root'); // Replace with your MySQL username
-define('DB_PASS', '');     // Replace with your MySQL password
-
-// Stripe API keys
-define('STRIPE_SECRET_KEY', 'sk_test_51OXpKeDss4ge2svjj91fMoX1sviWjXj2NJA4mKlSrlutOJoic3ncoJ1IfjKRrkZWP1eAPPezIaSmRIcVJEBXrhhR00wBVE9YUr');
-define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51OXpKeDss4ge2svjzx05mbmCWuGMx3lXKXLAuQjFGkQMhSel2JtwIFP1bV96Wdl0i8c3yDpycWlGtuhn7u9peRp200pXVJatdC');
